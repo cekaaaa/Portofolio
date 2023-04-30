8c77269eb1db267516f1a085856aa3be1f6765a2
cekaaaa <92358090+cekaaaa@users.noreply.github.com>
2023-04-30 21:37:28 +0700

    express js

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require("express"); 
+
+const path = require('path'); 
+
+const app = express(); 
+
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "./index.html"));
+}) 
+
+const port = process.env.PORT || 3000; 
+
+app.use(express.static(path.join(__dirname, "/")));
+
+app.listen(port, (_) => {
+    console.log(`Deployed at port ${port}`)
+})
